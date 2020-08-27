48a0fa7f6541304eb5a7d9060586d6984e7d429e
saturi11 <gabrielsaturi11@gmail.com>
2020-08-26 21:34:48 -0300

    ex7 pronto , mas sem cauculo

--- lista1-ex7.js
+++ lista1-ex7.js
@@ -1,1 +1,17 @@
-console.log('ex7');
+chefe();
+function chefe(){
+    var vet =[];
+    ex7(vet)
+}
+
+function ex7(vet){
+    var numero = Number (prompt('informe o numero '))
+    do {
+        vet.push(numero)
+         numero = Number (prompt('informe o numero, informe 0 para sair  '))
+
+
+    }
+    while(numero >= 0)
+
+}
